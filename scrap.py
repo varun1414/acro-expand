94175ad223d3b576ac3e88914506e50de467e395
varun <naik.varun99@gmail.com>
2020-11-23 12:39:34 +0530

    added the replace function

--- scrap.py
+++ scrap.py
@@ -2,10 +2,11 @@ import bs4
 import requests
 import re
 from filter import sep_upper
+import json
 
 extract = bs4.BeautifulSoup(requests.get('https://www.verywellfamily.com/the-secret-language-of-teens-100-social-media-acronyms-2609651').content, 'html.parser')
 elems = extract.select('strong')
-# print(elems)
+
 unfiltered=[]
 for i in elems:
     unfiltered.append(i.getText())
@@ -13,6 +14,20 @@ un_string=" ".join(unfiltered)
 un_string=un_string.replace(u'\xa0',u' ')
 un_string=un_string.strip()
 un_array=un_string.split('-')
-# print(un_array)
 
-sep_upper(un_array)
+""" 
+uncomment following to see web scarpping part 
+"""
+# sep_upper(un_array)
+
+def replace_acro(acro):
+    with open("main_final.json", "r") as read_file:
+        data = json.load(read_file)
+    
+    if acro in data:
+        return data[acro]
+    else:
+        return -1
+
+
+print(replace_acro('gal'))
